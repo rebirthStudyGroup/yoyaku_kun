800235374bc47aec1bcb85b8ef9d624ee166fb68
akatsu-h <akatsu.h@re-birth-it.jp>
2020-05-09 20:08:30 +0900

    DBバックアップシェルにエクスポートオプションを追加。インポートシェルを作成

---
+++ batch/yoyaku_import.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+# gz形式で圧縮されたDumpファイルをインポートするバッチ、引数にファイルのパスを指定してください。
+
+DIR=$(cd $(dirname $0); pwd)
+
+LOGDIR=$DIR/log/yoyaku_kun_import_`date '+%Y%m%d%H%M%S'`.log
+
+mkdir -p $DIR/log
+
+
+if [ $# -eq 0 ]; then
+    echo "specify dump file"
+    exit
+fi
+if [ $# -eq 1 ]; then
+    targetfile=$1
+fi
+
+echo $targetfile >> $LOGDIR
+
+
+echo "zcat ${targetfile} | mysql -h 127.0.0.1 -u root -pPassword res_system" >> $LOGDIR
+zcat ${targetfile} | mysql -h 127.0.0.1 -u root -pPassword res_system
+
+
+
+
+
+
+
